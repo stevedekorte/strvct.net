aba551bd1e3c50d8f76b1b27fbe2e7b0457a2c31
Steve Dekorte <steve@dekorte.com>
2024-01-24 15:15:35 -0800

    main.js typo fix

--- local-web-server/main.js
+++ local-web-server/main.js
@@ -7,7 +7,7 @@ const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const argv = yargs(hideBin(process.argv)).options({
   port: { type: 'number', demandOption: false, describe: 'Port number' },
-  key: { type: 'stri//ng', demandOption: false, describe: 'Key file path' },
+  key: { type: 'string', demandOption: false, describe: 'Key file path' },
   cert: { type: 'string', demandOption: false, describe: 'Cert file path' },
   secure: { type: 'boolean', demandOption: false, default:false, describe: 'Is secure (set to true for HTTPS, false for HTTP)' }
 }).argv;
