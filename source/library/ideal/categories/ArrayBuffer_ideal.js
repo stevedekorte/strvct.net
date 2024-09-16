1de924fff991584d533cb8928ab12c0fd7492cd9
Steve Dekorte <steve@dekorte.com>
2024-09-16 12:09:37 -0700

    docs work

--- source/library/ideal/categories/ArrayBuffer_ideal.js
+++ source/library/ideal/categories/ArrayBuffer_ideal.js
@@ -1,15 +1,18 @@
 "use strict";
 
-/*
-
-    ArrayBuffer_ideal
-
-    Some extra methods for the Javascript ArrayBuffer primitive.
-
-*/
-
+/**
+ * ArrayBuffer_ideal
+ * 
+ * Extends the JavaScript ArrayBuffer primitive with additional methods.
+ * @extends ArrayBuffer
+ */
 (class ArrayBuffer_ideal extends ArrayBuffer {
 
+    /**
+     * Converts the ArrayBuffer to a string using UTF-8 encoding.
+     * @returns {string} The decoded string representation of the ArrayBuffer.
+     * @throws {Error} If the bytes in the ArrayBuffer are not valid UTF-8.
+     */
     asString () {
         // have to be careful with this.  If the bytes are not valid utf-8, this will throw an error.
         return new TextDecoder().decode(this);
--- source/library/node/storage/base/categories/primitives/ArrayBuffer_store.js
+++ source/library/node/storage/base/categories/primitives/ArrayBuffer_store.js
@@ -1,8 +1,17 @@
 "use strict";
 
-
+/**
+ * Extends the ArrayBuffer class with storage-related functionality.
+ * @extends ArrayBuffer
+ */
 (class ArrayBuffer_store extends ArrayBuffer {
 
+    /**
+     * Creates an instance of ArrayBuffer_store from a record in the store.
+     * @param {Object} aRecord - The record containing the ArrayBuffer data.
+     * @param {Object} aStore - The store object.
+     * @returns {ArrayBuffer_store} A new instance of ArrayBuffer_store.
+     */
     static instanceFromRecordInStore (aRecord, aStore) { // should only be called by Store
         //assert(aRecord.type === "ArrayBuffer")
         const bytes = aRecord.bytes
@@ -10,6 +19,12 @@
         return obj
     }
 
+    /**
+     * Loads data from a record into this ArrayBuffer_store instance.
+     * @param {Object} aRecord - The record containing the ArrayBuffer data.
+     * @param {Object} aStore - The store object.
+     * @returns {ArrayBuffer_store} This instance after loading the data.
+     */
     loadFromRecord (aRecord, aStore) {
         assert(aRecord.bytes.length === this.length)
         const bytes = aRecord.bytes
@@ -19,6 +34,10 @@
         return this
     }
 
+    /**
+     * Converts the ArrayBuffer_store to an array of bytes.
+     * @returns {number[]} An array of byte values.
+     */
     bytes () {
         const bytes = []
         for (let i = 0; i < this.byteLength; i++) {
@@ -27,6 +46,11 @@
         return bytes
     }
 
+    /**
+     * Creates a record representation of this ArrayBuffer_store for storage.
+     * @param {Object} aStore - The store object.
+     * @returns {Object} A record object representing this ArrayBuffer_store.
+     */
     recordForStore (aStore) { // should only be called by Store
         return {
             type: "ArrayBuffer", //Type.typeName(this), 
@@ -34,6 +58,11 @@
         }
     }
 
+    /**
+     * Returns a Set of persistent unique identifiers (PUIDs) for this ArrayBuffer_store.
+     * @param {Set<string>} [puuids=new Set()] - A Set to store the PUIDs.
+     * @returns {Set<string>} The Set of PUIDs.
+     */
     refsPidsForJsonStore (puuids = new Set()) {
         return puuids
     }
